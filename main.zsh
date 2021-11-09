21cc0cfcd45afbc6bd86c96da6f9c05910edeef9
Adam Chalmers <adamschalmers@gmail.com>
2021-11-09 15:59:19 -0600

    Add zsh main

---
+++ main.zsh
@@ -0,0 +1,17 @@
+alias g="git"
+alias gc="git commit"
+alias gs="git status"
+alias gpf="git push --force-with-lease"
+alias c="cargo"
+alias k="kubectl"
+export PATH="/Applications/Sublime Text.app/Contents/SharedSupport/bin:$PATH"
+export PATH="/usr/local/opt/llvm@8/bin:$PATH"
+DEFAULT_USER=$USER
+
+# fnm
+export PATH="/var/folders/4j/r1kf44dx2xgd5ct6snpxpfy00000gn/T/fnm_multishells/46371_1636061730326/bin":$PATH
+export FNM_MULTISHELL_PATH="/var/folders/4j/r1kf44dx2xgd5ct6snpxpfy00000gn/T/fnm_multishells/46371_1636061730326"
+export FNM_DIR="/Users/adamchalmers/.fnm"
+export FNM_LOGLEVEL="info"
+export FNM_NODE_DIST_MIRROR="https://nodejs.org/dist"
+export FNM_ARCH="x64"
