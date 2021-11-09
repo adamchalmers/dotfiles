6da7bdab23307947dc8ca1189b9dd45493df8ecc
Adam Chalmers <adamschalmers@gmail.com>
2021-11-09 16:19:11 -0600

    Add vscode settings and a setup script

---
+++ setup.sh
@@ -0,0 +1,3 @@
+ln -s ~/programming/dotfiles/vscode.json ~/Library/Application\ Support/Code/User/settings.json
+ln -s ~/programming/dotfiles/.vimrc ~/.vimrc   
+ln -s ~/programming/dotfiles/main.zsh ~/.oh-my-zsh/custom
