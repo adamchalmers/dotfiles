d1722d2777d76c063aa54de66661c526a0f37252
Adam Chalmers <adamschalmers@gmail.com>
2021-11-23 16:57:26 -0600

    Tweaks

--- setup.sh
+++ setup.sh
@@ -6,5 +6,5 @@ brew install fnm
 
 # Link dotfiles to the right place
 ln -s ~/programming/dotfiles/vscode.json ~/Library/Application\ Support/Code/User/settings.json
-ln -s ~/programming/dotfiles/.vimrc ~/.vimrc   
+ln -s ~/programming/dotfiles/vimrc ~/.vimrc   
 ln -s ~/programming/dotfiles/main.zsh ~/.oh-my-zsh/custom
